3be9649ae63fed17844874ddf1f4547e23008591
Mim Hufford <mim@hotmail.co.uk>
2020-03-02 19:50:59 +0000

    video mode, clear screen, draw pixel

--- flapg.asm
+++ flapg.asm
@@ -1,17 +1,39 @@
-mov ah, 0x0e
+%define WIDTH     320
+%define HEIGHT    200
+%define VGA_MEM   0xA000
+%define VGA_SIZE  64000 ; 320*200
+%define GRAVITY   1
+%define JUMP_POW  20
+%define BIRD_SIZE 10
+%define BIRD_X    30
+%define BIRD_COL  0
+%define BG_COL    2
 
-mov al, 'F'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'a'
-int 0x10
-mov al, 'p'
-int 0x10
-mov al, 'G'
+; Enter VGA mode
+; 320x200x8-bits/pixel
+mov ax, 0x13
 int 0x10
 
+clear_screen:
+    mov ax, VGA_MEM
+    mov es, ax
+    mov bx, 0
+	clear_screen_inner:
+        mov BYTE [es:bx], BG_COL
+        inc bx
+        cmp bx, VGA_SIZE
+        jb clear_screen_inner
+
+; TODO: drawing single pixel atm, need to loop for BIRD_SIZE
+draw_bird:
+    mov ax, VGA_MEM
+    mov es, ax
+    mov BYTE [es:10*WIDTH+BIRD_X], BIRD_COL
+
+; TODO: handle keyboard, just loop forever for now
 jmp $
+
+bird_y: dw 100 ; starting bird y position
 
 times 510 - ($-$$) db 0
 dw 0xaa55
